0eeb3f803ace9d2305f7064466ab9e6c7f7b8d7a
Alexey Horoshavin <Akela.Klimov@yandex.ru>
2021-11-24 13:54:17 +0700

    fix soma app functions

--- wheretogoclient/app/src/main/java/com/example/wheretogo/ui/MainActivity.java
+++ wheretogoclient/app/src/main/java/com/example/wheretogo/ui/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.wheretogo.R;
-import com.example.wheretogo.ui.map.MapsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,12 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void openMap(View view){
-        //TODO:дописать класс отображения карты
-        Intent intent = new Intent(this, MapsActivity.class);
-        startActivity(intent);
     }
 
     public void openLogin(View view){
