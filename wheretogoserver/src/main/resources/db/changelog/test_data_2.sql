ff038ee91be5d1aec0c64333521aef73bae70f10
Alexey Horoshavin <Akela.Klimov@yandex.ru>
2022-04-15 16:00:33 +0700

    добавлены скрипты дополнительных мест в БД

---
+++ wheretogoserver/src/main/resources/db/changelog/test_data_2.sql
@@ -0,0 +1,364 @@
+INSERT INTO place (
+    id,
+    name,
+    description,
+    latitude,
+    longitude,
+    thumbnail,
+    uploaded_at
+) VALUES (
+             11,
+             'Архитектурное наслаждение',
+             'Красивое здание от Дмитрия Ощепкова на базе отдыха. Проезд на машине возможен прямо до этой локации. Город Новосибирск.',
+             55.05074,
+             82.83291,
+             'https://static.tildacdn.com/tild6238-3336-4439-a237-623064666564/42.png',
+             CURRENT_DATE
+         );
+insert into place_category values (11, 5);
+insert into place_category values (11, 6);
+
+INSERT INTO place (
+    id,
+    name,
+    description,
+    latitude,
+    longitude,
+    thumbnail,
+    uploaded_at
+) VALUES (
+             12,
+             'Безграничность',
+             'Живописный обрыв в Бердске. Подъезд к месту на машине невозможен. Нужно оставить машину рядом с локацией и немного пройти пешком',
+             54.73981,
+             83.02137,
+             'https://static.tildacdn.com/tild3135-3331-4666-b333-663035313339/41.png',
+             CURRENT_DATE
+         );
+insert into place_category values (12, 4);
+insert into place_category values (12, 7);
+
+INSERT INTO place (
+    id,
+    name,
+    description,
+    latitude,
+    longitude,
+    thumbnail,
+    uploaded_at
+) VALUES (
+             13,
+             'Безлюдный обрыв',
+             'Живописный обрыв в Бердске. Проезд на машине возможен прямо до этой локации',
+             54.77551,
+             83.06765,
+             'https://static.tildacdn.com/tild3935-3931-4135-a134-356238656463/39.png',
+             CURRENT_DATE
+         );
+
+insert into place_category values (13, 4);
+insert into place_category values (13, 7);
+
+INSERT INTO place (
+    id,
+    name,
+    description,
+    latitude,
+    longitude,
+    thumbnail,
+    uploaded_at
+) VALUES (
+             14,
+             'Белая соль',
+             'Склад мешков в Новосибирске. ЛОКАЦИЯ ВРЕМЕННАЯ: возможно, эти пакеты уже увезли. Если решите проверить, то напишите, пожалуйста, об актуальности данной локации!',
+             55.03242,
+             82.9189,
+             'https://static.tildacdn.com/tild3237-3532-4734-b661-616666653562/116.png',
+             CURRENT_DATE
+         );
+
+insert into place_category values (14, 2);
+insert into place_category values (14, 6);
+
+INSERT INTO place (
+    id,
+    name,
+    description,
+    latitude,
+    longitude,
+    thumbnail,
+    uploaded_at
+) VALUES (
+             15,
+             'Белена',
+             'Пляжные сооружения в Бердске. Проезд на машине возможен прямо до этой локации!',
+             54.77589,
+             83.06624,
+             'https://static.tildacdn.com/tild6661-3137-4634-b130-613065663131/162.png',
+             CURRENT_DATE
+         );
+
+insert into place_category values (15, 2);
+insert into place_category values (15, 4);
+insert into place_category values (15, 6);
+
+INSERT INTO place (
+    id,
+    name,
+    description,
+    latitude,
+    longitude,
+    thumbnail,
+    uploaded_at
+) VALUES (
+             16,
+             'Брошенный остров',
+             'Остров с постройками в Новосибирске. Подъезд к месту на машине невозможен. Проход только по льду либо на лодке. Новосибирск, 20 км от центра города',
+             54.84949,
+             83.00605,
+             'https://static.tildacdn.com/tild3563-6330-4631-b236-393238356163/___1.png',
+             CURRENT_DATE
+         );
+
+insert into place_category values (16, 1);
+insert into place_category values (16, 2);
+insert into place_category values (16, 4);
+insert into place_category values (16, 6);
+
+INSERT INTO place (
+    id,
+    name,
+    description,
+    latitude,
+    longitude,
+    thumbnail,
+    uploaded_at
+) VALUES (
+             17,
+             'Вечное зарево',
+             'Яркие парники в Новосибирске. Подъезд к месту на машине возможен.',
+             55.01636,
+             82.94845,
+             'https://static.tildacdn.com/tild3563-6330-4631-b236-393238356163/___1.png',
+             CURRENT_DATE
+         );
+
+insert into place_category values (17, 3);
+insert into place_category values (17, 2);
+insert into place_category values (17, 6);
+
+INSERT INTO place (
+    id,
+    name,
+    description,
+    latitude,
+    longitude,
+    thumbnail,
+    uploaded_at
+) VALUES (
+             18,
+             'В мире литеартуры',
+             'Читальные залы в Новосибирске. Помещение с научной литературой, библиотекари разрешают фотографировать. Подъезд к месту на машине возможен.',
+             54.98786,
+             82.90916,
+             'https://static.tildacdn.com/tild6163-3334-4363-b865-616666613434/77.png',
+             CURRENT_DATE
+         );
+
+insert into place_category values (18, 5);
+insert into place_category values (18, 6);
+
+INSERT INTO place (
+    id,
+    name,
+    description,
+    latitude,
+    longitude,
+    thumbnail,
+    uploaded_at
+) VALUES (
+             19,
+             'В окружении красок',
+             'Лучшая точка для просмотра салюта в Новосибирске. Советуем вам смотреть салют с воды. на каждый городской праздник с Михайловской набережной ходят экскурсионные катеры.',
+             55.00984,
+             82.92864,
+             'https://static.tildacdn.com/tild3637-3836-4561-a166-663838326666/123.png',
+             CURRENT_DATE
+         );
+
+insert into place_category values (19, 3);
+insert into place_category values (19, 6);
+
+INSERT INTO place (
+    id,
+    name,
+    description,
+    latitude,
+    longitude,
+    thumbnail,
+    uploaded_at
+) VALUES (
+             20,
+             'Геометрия',
+             'Ход с колоннами в Новосибирске. Проезд на машине возможен до этой локации.',
+             55.01511,
+             82.93302,
+             'https://static.tildacdn.com/tild3736-3434-4861-a231-646464636562/192.png',
+             CURRENT_DATE
+         );
+
+insert into place_category values (20, 5);
+insert into place_category values (20, 6);
+
+INSERT INTO place (
+    id,
+    name,
+    description,
+    latitude,
+    longitude,
+    thumbnail,
+    uploaded_at
+) VALUES (
+             21,
+             'Весенний контраст',
+             'Спортивная площадка в Новосибирске. Проезд на машине возможен до этой локации.',
+             55.04506,
+             82.95837,
+             'https://static.tildacdn.com/tild6234-6639-4632-a661-616364326563/144.png',
+             CURRENT_DATE
+         );
+
+insert into place_category values (21, 5);
+insert into place_category values (21, 6);
+
+INSERT INTO place (
+    id,
+    name,
+    description,
+    latitude,
+    longitude,
+    thumbnail,
+    uploaded_at
+) VALUES (
+             22,
+             'Второе самое красивое место',
+             'Чистый печаный пляж в Новосибирске. Проезд на машине возможен до этой локации.',
+             54.99899,
+             82.93048,
+             'https://static.tildacdn.com/tild3331-3533-4134-b737-643639373239/5.png',
+             CURRENT_DATE
+         );
+
+insert into place_category values (22, 4);
+insert into place_category values (22, 6);
+insert into place_category values (22, 2);
+
+INSERT INTO place (
+    id,
+    name,
+    description,
+    latitude,
+    longitude,
+    thumbnail,
+    uploaded_at
+) VALUES (
+             23,
+             'Действующая психиатрическая больница',
+             'Корпуса больницы в Новосибирске. Стиль арт-хаоса. Проезд на машине возможен до этой локации.',
+             55.03539,
+             82.88506,
+             'https://static.tildacdn.com/tild6637-3338-4162-a630-633162303236/17.png',
+             CURRENT_DATE
+         );
+
+insert into place_category values (23, 5);
+insert into place_category values (23, 6);
+
+INSERT INTO place (
+    id,
+    name,
+    description,
+    latitude,
+    longitude,
+    thumbnail,
+    uploaded_at
+) VALUES (
+             24,
+             'Громадный шар посреди поля',
+             'Доплеровский радар в Новосибирске в Новосибирске. Стиль арт-хаоса. Проезд на машине возможен до этой локации.',
+             55.06552,
+             83.04769,
+             'https://static.tildacdn.com/tild3936-6237-4764-b062-316337306136/3.png',
+             CURRENT_DATE
+         );
+
+insert into place_category values (24, 1);
+insert into place_category values (24, 2);
+insert into place_category values (24, 6);
+
+INSERT INTO place (
+    id,
+    name,
+    description,
+    latitude,
+    longitude,
+    thumbnail,
+    uploaded_at
+) VALUES (
+             26,
+             'Единение с природой',
+             'Поле папоротника в Новосибирске. Проезд на машине возможен до этой локации.',
+             55.04757,
+             82.95274,
+             'https://static.tildacdn.com/tild3034-3733-4463-a564-636364626561/184.png',
+             CURRENT_DATE
+         );
+
+insert into place_category values (26, 4);
+insert into place_category values (26, 6);
+
+INSERT INTO place (
+    id,
+    name,
+    description,
+    latitude,
+    longitude,
+    thumbnail,
+    uploaded_at
+) VALUES (
+             27,
+             'Восстановление',
+             'Строительство церкви в Новосибирске. Проезд на машине возможен до этой локации.',
+             55.02222,
+             82.97971,
+             'https://static.tildacdn.com/tild6639-3534-4031-b335-613238613036/31.png',
+             CURRENT_DATE
+         );
+
+insert into place_category values (27, 5);
+insert into place_category values (27, 6);
+
+INSERT INTO place (
+    id,
+    name,
+    description,
+    latitude,
+    longitude,
+    thumbnail,
+    uploaded_at
+) VALUES (
+             28,
+             'Масштабная забытость',
+             'Брошенный бассейн в новосибирске в Новосибирске. Вход может быть затруднен. Могу дежурить ОХРАННИКИ!',
+             55.01383,
+             82.97549,
+             'https://static.tildacdn.com/tild6537-3734-4862-a661-306639613762/113.png',
+             CURRENT_DATE
+         );
+
+insert into place_category values (28, 1);
+insert into place_category values (28, 6);
+
+
+
+
