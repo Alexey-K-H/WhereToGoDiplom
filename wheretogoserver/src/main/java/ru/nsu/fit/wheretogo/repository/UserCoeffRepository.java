fb3edcda511219f4fd1a3fc705fc80331bd80853
Alexey Horoshavin <Akela.Klimov@yandex.ru>
2022-06-19 14:24:13 +0700

    fixes

--- wheretogoserver/src/main/java/ru/nsu/fit/wheretogo/repository/UserCoeffRepository.java
+++ wheretogoserver/src/main/java/ru/nsu/fit/wheretogo/repository/UserCoeffRepository.java
@@ -19,8 +19,8 @@ public interface UserCoeffRepository extends JpaRepository<UserCoefficient, Long
 
     //Обновление коэффициента с добавлением нового места
     @Modifying
-    @Query(value = "update user_coeff u set u.coeff = ((u.coeff) + (:_new_score/u.count_places))*(u.count_places/(u.count_places+1)), u.count_places = u.count_places + 1 " +
-            "where u.category_id = :_category_id and u.user_id = :_user_id", nativeQuery = true)
+    @Query(value = "update where_to_go.user_coeff set coeff = ((coeff) + (:_new_score/count_places))*(count_places/(count_places+1)), count_places = count_places + 1 " +
+            "where category_id = :_category_id and user_id = :_user_id", nativeQuery = true)
     void updateCoeffWithInc(
             @Param("_new_score") Double newScore,
             @Param("_category_id") Integer categoryId,
@@ -29,8 +29,8 @@ public interface UserCoeffRepository extends JpaRepository<UserCoefficient, Long
 
     //Обновление коэффициента без добавления нового места
     @Modifying
-    @Query(value = "update user_coeff u set u.coeff = ((u.coeff) + (:_new_score/u.count_places))" +
-            "where u.category_id = :_category_id and u.user_id = :_user_id", nativeQuery = true)
+    @Query(value = "update where_to_go.user_coeff set coeff = ((coeff) + (:_new_score/count_places))" +
+            "where category_id = :_category_id and user_id = :_user_id", nativeQuery = true)
     void updateCoeffWitOutInc(
             @Param("_new_score") Double newScore,
             @Param("_category_id") Integer categoryId,
