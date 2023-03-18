2cee1dfa3b321a969aadd6f8f58c374e462be707
Aleksey <43428247+aleksey-khoroshavin@users.noreply.github.com>
2023-03-18 21:24:14 +0700

    WHERETOGO-0003 Обновлен CI для проекта

--- where_to_go_back/src/main/java/ru/nsu/fit/wheretogo/WhereToGoApplication.java
+++ where_to_go_back/src/main/java/ru/nsu/fit/wheretogo/WhereToGoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WhereToGoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WhereToGoApplication.class, args);
     }
-
 }
