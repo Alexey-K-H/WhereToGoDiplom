dcacc5645be09bd1c8799c32b67382303aec8bd4
Хорошавин Алексей Константинович <Akela.Klimov@yandex.ru>
2024-03-10 20:45:43 +0700

    Добавлены места в БД - текущее число тестовых мест: 51

---
+++ where-to-go-back/src/main/resources/db/migration/V24__Add_Places_Data.sql
@@ -0,0 +1,545 @@
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        28,
+        'Фактурная стена',
+        'Облупленная краска в Новосибирске',
+        55.0189722,
+        82.9261667,
+        'https://static.tildacdn.com/tild6637-3166-4232-b533-313864386337/200.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (28, 5);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (28, 6);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        29,
+        'Neon',
+        'Неоновые высотки в Новосибирске',
+        55.0356667,
+        82.9013889,
+        'https://static.tildacdn.com/tild6363-6566-4131-b833-363566623637/198.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (29, 3);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (29, 5);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (29, 6);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        30,
+        'Бескрайние поля',
+        'Поля без горизонта в Бердске',
+        54.7846667,
+        83.0968889,
+        'https://static.tildacdn.com/tild3734-3134-4131-b138-396365306239/153.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (30, 4);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (30, 7);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        31,
+        'Брошенный амфитеатр',
+        'Брошенный мраморный карьер в НСО',
+        54.6012,
+        83.3421013,
+        'https://static.tildacdn.com/tild6632-6236-4864-b764-333731646636/87.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (31, 2);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (31, 7);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        32,
+        'Бугринский мост',
+        'Смотровая площадка в Новосибирске',
+        54.9723473,
+        82.9564653,
+        'https://static.tildacdn.com/tild6163-6638-4631-a135-623830646465/150.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (32, 2);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (32, 6);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        33,
+        'Весенние приключения',
+        'Заброшенный пирс в Бердске',
+        54.7846667,
+        83.0968889,
+        'https://static.tildacdn.com/tild3264-3130-4836-b335-306433386237/152.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (33, 1);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (33, 2);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (33, 4);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (33, 7);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        34,
+        'Взрыв красок',
+        'Яркая глина в прозрачном озере в НСО',
+        54.2288611,
+        81.7111389,
+        'https://static.tildacdn.com/tild3230-3236-4939-a362-343465623233/102.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (34, 4);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (34, 7);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        35,
+        'Внутри старинной башни',
+        'Заброшенная башня в Бердске',
+        54.735,
+        83.0582778,
+        'https://static.tildacdn.com/tild3237-3365-4665-b561-393731376237/___1.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (35, 1);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (35, 7);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        36,
+        'Внутри старой башни',
+        'Недостроенный водозабор в Бердске',
+        54.7128889,
+        83.0197778,
+        'https://static.tildacdn.com/tild3062-3039-4633-b565-633538303863/13.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (36, 1);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (36, 2);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (36, 7);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        37,
+        'Водные мощи',
+        'Заброшенная ГЭС в НСО',
+        55.2442836,
+        84.7134441,
+        'https://static.tildacdn.com/tild6262-3738-4231-b433-626435633661/___1.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (37, 1);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (37, 2);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (37, 4);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (37, 7);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        38,
+        'Водопад и голубое озеро',
+        'Водопад и голубое озеро в НСО',
+        54.5598056,
+        83.6206944,
+        'https://static.tildacdn.com/tild3437-6335-4738-b338-373461633962/73.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (38, 4);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (38, 7);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        39,
+        'Военный городок, ожидающий реновации',
+        'Комплекс заброшенных военных зданий в Новосибирске',
+        54.7925278,
+        83.0982778,
+        'https://static.tildacdn.com/tild3461-3266-4834-b630-343365323235/28.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (39, 1);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (39, 5);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (39, 6);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        40,
+        'Второй повод съездить в Искитим',
+        'Дырявая водонапорная башня в НСО',
+        54.6094722,
+        83.3815833,
+        'https://static.tildacdn.com/tild6135-6566-4135-b732-663730633737/89.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (40, 2);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (40, 7);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        41,
+        'Выше леса',
+        'Геодезическая вышка в Новосибирске',
+        54.8367778,
+        83.0720556,
+        'https://static.tildacdn.com/tild3764-6438-4231-a565-626236376637/94.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (41, 2);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (41, 6);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        42,
+        'Головоломка',
+        'Гора бетонных плит в Новосибирске',
+        55.0163895,
+        82.91467,
+        'https://static.tildacdn.com/tild3133-3363-4934-b535-633437353936/___1.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (42, 2);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (42, 6);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        43,
+        'Городской колорит',
+        'Белая стена с лестницей в Новосибирске',
+        54.9915833,
+        82.9726944,
+        'https://static.tildacdn.com/tild3030-3639-4664-a337-616132663931/147.pngя',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (43, 5);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (43, 6);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        44,
+        'Гранд каньон',
+        'Огромный котлован в Новосибирске',
+        54.942781,
+        82.7977165,
+        'https://static.tildacdn.com/tild3165-3031-4833-a531-643061323731/6.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (44, 2);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (44, 6);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        45,
+        'Громадное ограждение',
+        'Заводская стена в Новосибирске',
+        55.0117222,
+        82.8470278,
+        'https://static.tildacdn.com/tild6437-3536-4538-b639-363535643564/37.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (45, 2);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (45, 6);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        46,
+        'Доброе утро!',
+        'Огромный пирс в Новосибирске',
+        54.83325,
+        83.0508889,
+        'https://static.tildacdn.com/tild6233-6135-4566-a432-323533333437/167.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (46, 4);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (46, 6);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        47,
+        'Дом культуры из прошлого',
+        'Увядающее здание в Новосибирске',
+        54.8155833,
+        82.8525278,
+        'https://static.tildacdn.com/tild6537-3937-4534-b736-313630373738/20.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (47, 5);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (47, 6);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        48,
+        'Дорога к Чумикану',
+        'Заброшенный корабль в Новосибирске',
+        54.8420036,
+        83.0539521,
+        'https://static.tildacdn.com/tild3261-6132-4666-b432-636533626234/___1.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (48, 2);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (48, 4);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (48, 6);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        49,
+        'Доступная спортивная площадка',
+        'Открытая спортивная площадка в Новосибирске',
+        54.9340238,
+        83.203693,
+        'https://static.tildacdn.com/tild3762-3833-4366-a333-323065353961/2.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (49, 2);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (49, 5);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (49, 6);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        50,
+        'Другая планета',
+        'Горы извести в НСО',
+        55.1054404,
+        83.803005,
+        'https://static.tildacdn.com/tild6163-3032-4331-a432-323632386137/___1.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (50, 2);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (50, 7);
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        51,
+        'Дюны',
+        'Горы песка в НСО',
+        54.3598056,
+        81.9431944,
+        'https://static.tildacdn.com/tild6235-6136-4335-b132-386333633866/___1.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (51, 4);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (51, 6);
