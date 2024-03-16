005a23a127863c2b954d00bf8b985cb5b80ef7a3
Хорошавин Алексей Константинович <Akela.Klimov@yandex.ru>
2024-03-16 10:59:33 +0700

    Добавлены места в БД - текущее число тестовых мест: 204/204

---
+++ where-to-go-back/src/main/resources/db/migration/V29__Add_Places_Data.sql
@@ -0,0 +1,505 @@
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        185,
+        'Территория постоянной радости',
+        'Сухой фонтан в Новосибирске',
+        55.0343333,
+        82.9246111,
+        'https://static.tildacdn.com/tild3031-3834-4435-a661-363937633661/85.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (185, 2);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (185, 6);
+
+
+UPDATE T_PLACE
+SET DURATION = CREATE_RANDOM_DURATION()
+WHERE ID = 185;
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        186,
+        'Тихий пляж',
+        'Песчаный пляж рядом с обрывами в Бердске',
+        54.7221944,
+        83.02225,
+        'https://static.tildacdn.com/tild6637-6233-4433-a530-646438383634/34.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (186, 2);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (186, 4);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (186, 7);
+
+UPDATE T_PLACE
+SET DURATION = CREATE_RANDOM_DURATION()
+WHERE ID = 186;
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        187,
+        'Третий повод съездить в Искитим',
+        'Деревенский быт в Искитиме',
+        54.6096944,
+        83.3924722,
+        'https://static.tildacdn.com/tild3637-6638-4566-a532-393939333034/88.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (187, 4);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (187, 7);
+
+UPDATE T_PLACE
+SET DURATION = CREATE_RANDOM_DURATION()
+WHERE ID = 187;
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        188,
+        'Тяжёлая артиллерия',
+        'Парковка грузовиков в Новосибирске',
+        54.9665278,
+        82.8113056,
+        'https://static.tildacdn.com/tild3139-6639-4837-b031-616235346233/56.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (188, 2);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (188, 3);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (188, 4);
+
+UPDATE T_PLACE
+SET DURATION = CREATE_RANDOM_DURATION()
+WHERE ID = 188;
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        189,
+        'Утёс',
+        'Красивая заводь в Бердске',
+        54.7615833,
+        83.2252222,
+        'https://static.tildacdn.com/tild3462-3365-4261-b037-313565666334/65.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (189, 2);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (189, 4);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (189, 7);
+
+UPDATE T_PLACE
+SET DURATION = CREATE_RANDOM_DURATION()
+WHERE ID = 189;
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        190,
+        'Ущелье',
+        'Большой ход в обрыве в Новосибирске',
+        54.8459722,
+        83.2111944,
+        'https://static.tildacdn.com/tild6266-3362-4536-a635-383732316661/81.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (190, 4);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (190, 6);
+
+UPDATE T_PLACE
+SET DURATION = CREATE_RANDOM_DURATION()
+WHERE ID = 190;
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        191,
+        'Феерическое представление',
+        'Современный ЖК в Новосибирске',
+        54.9911111,
+        82.9177778,
+        'https://static.tildacdn.com/tild6366-3339-4534-a131-376539613365/143.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (191, 5);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (191, 6);
+
+UPDATE T_PLACE
+SET DURATION = CREATE_RANDOM_DURATION()
+WHERE ID = 191;
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        192,
+        'Холодная луна',
+        'Яркая подсветка здания в Новосибирске',
+        55.0732338,
+        82.9091048,
+        'https://static.tildacdn.com/tild3739-6563-4939-b935-346437646537/___1.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (192, 3);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (192, 5);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (192, 6);
+
+UPDATE T_PLACE
+SET DURATION = CREATE_RANDOM_DURATION()
+WHERE ID = 192;
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        193,
+        'Царица льда',
+        'Ледяной городок в Новосибирске',
+        55.0063056,
+        82.9376944,
+        'https://static.tildacdn.com/tild3934-3964-4662-a530-663735393364/165.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (193, 2);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (193, 6);
+
+UPDATE T_PLACE
+SET DURATION = CREATE_RANDOM_DURATION()
+WHERE ID = 193;
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        194,
+        'Царство льда',
+        'Сооружения шлюза в Новосибирске',
+        54.8437222,
+        83.0401389,
+        'https://static.tildacdn.com/tild3038-3066-4465-b738-613562393265/155.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (194, 2);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (194, 6);
+
+UPDATE T_PLACE
+SET DURATION = CREATE_RANDOM_DURATION()
+WHERE ID = 194;
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        195,
+        'Цветник',
+        'Старый двор в Новосибирске',
+        54.9648889,
+        82.9183333,
+        'https://static.tildacdn.com/tild3534-3166-4837-b731-376234353938/98.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (195, 5);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (195, 6);
+
+UPDATE T_PLACE
+SET DURATION = CREATE_RANDOM_DURATION()
+WHERE ID = 195;
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        196,
+        'Штормовое предупреждение',
+        'Затопленный корабль в НСО',
+        54.5391197,
+        82.4693053,
+        'https://static.tildacdn.com/tild3735-6462-4737-a264-613638373635/___1.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (196, 1);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (196, 2);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (196, 7);
+
+UPDATE T_PLACE
+SET DURATION = CREATE_RANDOM_DURATION()
+WHERE ID = 196;
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        197,
+        'Это должен увидеть каждый',
+        'Снежная башня в Новосибирске',
+        54.8250556,
+        83.0829167,
+        'https://static.tildacdn.com/tild6639-6362-4336-b463-383736616165/160.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (197, 2);
+
+UPDATE T_PLACE
+SET DURATION = CREATE_RANDOM_DURATION()
+WHERE ID = 197;
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        198,
+        'Яркие моменты',
+        'Яркие контейнеры в Новосибирске',
+        54.9921667,
+        82.9016389,
+        'https://static.tildacdn.com/tild3765-6366-4232-a434-353235343232/196.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (198, 2);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (198, 6);
+
+UPDATE T_PLACE
+SET DURATION = CREATE_RANDOM_DURATION()
+WHERE ID = 198;
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        199,
+        'Яркие элементы',
+        'Яркая стена в Новосибирске',
+        55.068,
+        82.9128611,
+        'https://static.tildacdn.com/tild3639-3465-4530-b937-633735313464/66.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (199, 3);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (199, 6);
+
+UPDATE T_PLACE
+SET DURATION = CREATE_RANDOM_DURATION()
+WHERE ID = 199;
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        200,
+        'Яркий уголок',
+        'Яркая неоновая стена в Новосибирске',
+        55.01275,
+        82.9527778,
+        'https://static.tildacdn.com/tild6661-3131-4361-b437-376666303061/44.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (200, 3);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (200, 6);
+
+
+UPDATE T_PLACE
+SET DURATION = CREATE_RANDOM_DURATION()
+WHERE ID = 200;
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        201,
+        'Яркий элемент',
+        'Интерьер автомобильного салона в Новосибирске',
+        55.00275,
+        82.9480278,
+        'https://static.tildacdn.com/tild6638-6264-4833-a331-366631376139/46.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (201, 5);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (201, 6);
+
+UPDATE T_PLACE
+SET DURATION = CREATE_RANDOM_DURATION()
+WHERE ID = 201;
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        202,
+        'Яркое геометрическое наслаждение',
+        'Штаб квартира «Ленты» в Новосибирске',
+        55.0368889,
+        83.0128889,
+        'https://static.tildacdn.com/tild6362-6637-4266-a430-336366653838/51.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (202, 5);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (202, 6);
+
+UPDATE T_PLACE
+SET DURATION = CREATE_RANDOM_DURATION()
+WHERE ID = 202;
+
+INSERT INTO T_PLACE (
+                    ID,
+                    NAME,
+                    DESCRIPTION,
+                    LATITUDE,
+                    LONGITUDE,
+                    THUMBNAIL,
+                    UPLOADED_AT)
+VALUES (
+        203,
+        'Теплота',
+        'Беседки на пляже в Новосибирске',
+        54.8303056,
+        83.0798056,
+        'https://static.tildacdn.com/tild6530-3261-4634-b839-323761383139/172.png',
+        CURRENT_DATE);
+
+INSERT INTO T_PLACE_CATEGORY
+VALUES (203, 2);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (203, 4);
+INSERT INTO T_PLACE_CATEGORY
+VALUES (203, 6);
+
+UPDATE T_PLACE
+SET DURATION = CREATE_RANDOM_DURATION()
+WHERE ID = 203;
